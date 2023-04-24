35b32430ade4bbe547fe4e57f3e41c9b7bddd15b
Douglas Fugazi <itmteleco@gmail.com>
2023-04-24 13:24:42 -0500

    Updating BookingRest with Lombok entities

---
+++ src/test/java/com/api/tests/carbonlibre/bodys/BookingBody.java
@@ -0,0 +1,12 @@
+package com.api.tests.carbonlibre.bodys;
+
+import com.api.tests.carbonlibre.entities.Bookingdates;
+
+public class BookingBody {
+    private String firstname;
+    private String lastname;
+    private long totalprice;
+    private boolean depositpaid;
+    private Bookingdates bookingdates;
+    private String additionalneeds;
+}
---
+++ src/test/java/com/api/tests/carbonlibre/entities/Bookingdates.java
@@ -0,0 +1,9 @@
+package com.api.tests.carbonlibre.entities;
+
+import java.util.Date;
+
+@lombok.Data
+public class Bookingdates {
+    private Date checkin;
+    private Date checkout;
+}
