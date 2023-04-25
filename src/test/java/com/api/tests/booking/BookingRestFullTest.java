0366de8bd55bc7ad4755af9bfd7f872cd871c2b8
Douglas Fugazi <itmteleco@gmail.com>
2023-04-25 17:21:16 -0500

    BookingRestFullTest
    A new class to collect all tests and run all them together. WIP.

---
+++ src/test/java/com/api/tests/booking/BookingRestFullTest.java
@@ -0,0 +1,36 @@
+package com.api.tests.booking;
+
+import io.restassured.response.Response;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.api.tests.booking.bodys.AuthTokenBody;
+import com.api.tests.booking.bodys.BookingBody;
+import com.api.tests.booking.utility.CreateAuthTokenRequest;
+import com.api.tests.booking.utility.CreateBookingRequest;
+
+public class BookingRestFullTest {
+
+    /**
+     * This test will create an authorization token and verify that the token was created successfully.
+     * POST <a href="https://restful-booker.herokuapp.com/auth">/auth</a>
+     */
+    @Test(description = "Regression Test")
+    void createAuthorizationTokenTest() {
+        AuthTokenBody authTokenBody = AuthTokenBody.getInstance();
+        Response response = CreateAuthTokenRequest.newAuthTokenRequest(authTokenBody);
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
+    /**
+     * This test will create a booking and verify that the booking was created successfully.
+     * POST <a href="https://restful-booker.herokuapp.com/booking">/booking</a>
+     */
+    @Test(description = "Regression Test")
+    void createBookingTest() {
+        BookingBody bookingBody = BookingBody.getInstance();
+        Response response = CreateBookingRequest.newBookingRequest(bookingBody);
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+}
