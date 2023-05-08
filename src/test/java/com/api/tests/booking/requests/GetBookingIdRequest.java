02410df619e42c909079141378abbc4b69019640
Douglas Fugazi <itmteleco@gmail.com>
2023-05-07 19:56:14 -0500

    API Testing
    Updating tests

--- src/test/java/com/api/tests/booking/requests/GetBookingIdRequest.java
+++ src/test/java/com/api/tests/booking/requests/GetBookingIdRequest.java
@@ -6,7 +6,7 @@ import io.restassured.response.Response;
 
 public class GetBookingIdRequest {
     private static final String BASE_URI = "https://restful-booker.herokuapp.com";
-    private static final String BOOKING_ID = "1068";
+    private static final String BOOKING_ID = "1319";
 
     /**
      * This request will get a booking by id
--- src/test/java/com/api/tests/booking/requests/UpdateExistingBookingRequest.java
+++ src/test/java/com/api/tests/booking/requests/UpdateExistingBookingRequest.java
@@ -8,8 +8,8 @@ import com.api.tests.booking.bodys.BookingBodyUpdate;
 
 public class UpdateExistingBookingRequest {
     private static final String BASE_URI = "https://restful-booker.herokuapp.com";
-    private static final String TOKEN = "2d171bc7532ea09";
-    private static final String BOOKING_ID = "1068";
+    private static final String TOKEN = "e26c8325119f015";
+    private static final String BOOKING_ID = "1319";
 
     /**
      * This request will update an existing booking id
