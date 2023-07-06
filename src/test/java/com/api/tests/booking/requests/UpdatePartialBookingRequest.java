c088e141ee7070a1eb77196b7c75d98a6cd50289
Douglas Fugazi <itmteleco@gmail.com>
2023-07-06 16:28:00 -0500

    Update

--- src/test/java/com/api/tests/booking/requests/UpdatePartialBookingRequest.java
+++ src/test/java/com/api/tests/booking/requests/UpdatePartialBookingRequest.java
@@ -8,8 +8,8 @@ import com.api.tests.booking.bodys.BookingPartialBodyUpdate;
 
 public class UpdatePartialBookingRequest {
     private static final String BASE_URI = "https://restful-booker.herokuapp.com";
-    private static final String TOKEN = "51c98e0ae9fd33b";
-    private static final String BOOKING_ID = "662";
+    private static final String TOKEN = "f175d2dfbab3643";
+    private static final String BOOKING_ID = "1297";
 
     /**
      * This request will update an partial booking id
