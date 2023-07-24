00d253d6853e7c9738487d2093d8273c850fba30
Douglas Fugazi <itmteleco@gmail.com>
2023-07-23 19:39:32 -0500

    Updating tests
    Updating tests

--- src/test/java/com/api/tests/booking/requests/UpdateExistingBookingRequest.java
+++ src/test/java/com/api/tests/booking/requests/UpdateExistingBookingRequest.java
@@ -8,8 +8,8 @@ import com.api.tests.booking.bodys.BookingBodyUpdate;
 
 public class UpdateExistingBookingRequest {
     private static final String BASE_URI = "https://restful-booker.herokuapp.com";
-    private static final String TOKEN = "0bda4d834d08071";
-    private static final String BOOKING_ID = "1904";
+    private static final String TOKEN = "bcc28527c6dfb07";
+    private static final String BOOKING_ID = "1738";
 
     /**
      * This request will update an existing booking id
