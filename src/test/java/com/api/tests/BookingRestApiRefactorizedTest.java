ca4216b3fff61d32c788929fe83a0608d95b4a88
Douglas Fugazi <itmteleco@gmail.com>
2023-10-02 19:23:46 -0500

    BookingRestApiRefactorizedTest
    New BookingRestApiRefactorizedTest with all tested automatically

--- src/test/java/com/api/tests/BookingRestApiRefactorizedTest.java
+++ src/test/java/com/api/tests/BookingRestApiRefactorizedTest.java
@@ -11,18 +11,19 @@ import org.testng.annotations.Test;
 
 import com.api.tests.booking.baseSpec.BaseSpec;
 
-public class BookingRestApiAuthToken {
+public class BookingRestApiRefactorizedTest {
     private static String AUTH_TOKEN;
-    private static final String BOOKING_ID = "406";
-    private static final String DELETE_BOOKING_ID = "316";
+    private static String BOOKING_ID;
 
     @BeforeClass
     public static void setUp() {
         AUTH_TOKEN = obtainAuthorizationToken();
+        BOOKING_ID = obtainBookingIdFromGetAllBookings();
     }
 
     /**
      * Test to create an Authorization token for Booking API
+     *
      * @return AUTH_TOKEN
      */
     private static String obtainAuthorizationToken() {
@@ -42,13 +43,37 @@ public class BookingRestApiAuthToken {
     }
 
     /**
+     * Test to obtain all Bookings by IDs
+     * This test is tied to BaseSpec
+     *
+     * @return BOOKING_ID
+     * @see BaseSpec
+     */
+    private static String obtainBookingIdFromGetAllBookings() {
+        Response response = given()
+                .spec(BaseSpec.get().build())
+                .when().get("/booking")
+                .then().statusCode(200)
+                .and()
+                .assertThat().body("bookingid[0]", notNullValue())
+                .extract().response();
+
+        int bookingIdInt = response.path("bookingid[0]");
+        BOOKING_ID = String.valueOf(bookingIdInt);
+
+        return BOOKING_ID;
+    }
+
+    /**
      * Test to Get all Bookings by IDs
      * This test is tied to BaseSpec
+     *
      * @see BaseSpec
      * Verify the response against the JSON schema
      */
-    @Test(description = "Regression Test")
+    @Test(description = "Regression Test", priority = 1)
     void testGetAllBookings() {
+        // Este test captura el bookingid en obtainBookingIdFromGetAllBookings
         given()
                 .spec(BaseSpec.get().build())
                 .when().get("/booking")
@@ -62,10 +87,11 @@ public class BookingRestApiAuthToken {
     /**
      * Test to Get a Booking by ID
      * This test is tied to BaseSpec
+     *
      * @see BaseSpec
      * Verify the response against the JSON schema
      */
-    @Test(description = "Regression Test")
+    @Test(description = "Regression Test", priority = 2)
     void testGetBookingById() {
         given()
                 .spec(BaseSpec.get().build())
@@ -86,10 +112,11 @@ public class BookingRestApiAuthToken {
     /**
      * Test to Create a Booking
      * This test is tied to BaseSpec
+     *
      * @see BaseSpec
      * Verify the response against the JSON schema
      */
-    @Test(description = "Regression Test")
+    @Test(description = "Regression Test", priority = 3)
     void testCreateBooking() {
         given()
                 .spec(BaseSpec.get().build())
@@ -115,10 +142,11 @@ public class BookingRestApiAuthToken {
     /**
      * Test to Update a Booking
      * This test is tied to BaseSpec
+     *
      * @see BaseSpec
      * Verify the response against the JSON schema
      */
-    @Test(description = "Regression Test")
+    @Test(description = "Regression Test", priority = 4)
     void testUpdateBooking() {
         given()
                 .spec(BaseSpec.get(AUTH_TOKEN).build())
@@ -151,10 +179,11 @@ public class BookingRestApiAuthToken {
      * Test to Update a Booking with Partial Data
      * Call the VerifyRequestResponse class to verify the response
      * This test is tied to BaseSpec
+     *
      * @see BaseSpec
      * Verify the response against the JSON schema
      */
-    @Test(description = "Regression Test")
+    @Test(description = "Regression Test", priority = 5)
     void testUpdateBookingPartialData() {
         given()
                 .spec(BaseSpec.get(AUTH_TOKEN).build())
@@ -179,13 +208,14 @@ public class BookingRestApiAuthToken {
     /**
      * Test to Delete a Booking
      * This test is tied to BaseSpec
+     *
      * @see BaseSpec
      */
-    @Test(description = "Regression Test")
+    @Test(description = "Regression Test", priority = 6)
     void testDeleteBooking() {
         given()
                 .spec(BaseSpec.get(AUTH_TOKEN).build())
-                .when().delete("/booking/" + DELETE_BOOKING_ID)
+                .when().delete("/booking/" + BOOKING_ID)
                 .then().statusCode(201)
                 .and()
                 .assertThat().header("Content-Type", "text/plain; charset=utf-8")
